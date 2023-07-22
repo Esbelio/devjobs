698289739444fd7c80370f6eb7f93d8c16834f8c
esbelio <edgarestigarribia@gmail.com>
2023-07-22 06:45:54 -0400

    MostrarVacantes

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,17 +1,36 @@
 <div>
-    @foreach ($vacantes as $vacante)
+
+    @forelse ($vacantes as $vacante)
         
-        <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="p-6 text-gray-900 dark:text-gray-100 md:flex md:justify-between md:items-center">
             <div class="space-y-3">
                 <a href="#" class="text-xl font-bold">{{$vacante->titulo}}</a>
                 <p class="text-sm text-gray-600 font-bold">{{$vacante->empresa}} </p>
                 <p class="text-sm text-gray-500">Ultimo día: {{ $vacante->ultimo_dia->format('d/m/Y') }} </p>
             </div>
 
-            <div class="flex gap-3 items-center">
-                <a href="#">Lado Derecho</a>
+            <div class="flex flex-col md:flex-row items-stretch gap-3 mt-4 md:mt-0">
+                <a href="#" 
+                    class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                    Candidatos
+                </a>
+                
+                <a href="#" 
+                    class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                    Editar
+                </a>
+
+                <a href="#" 
+                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                    Eliminar
+                </a>
             </div>
+
         </div>
 
-    @endforeach
+    @empty
+
+        <p class="p-3 text-center text-sm text-gray-600">No hay Vacantes</p>
+        
+    @endforelse
 </div>
