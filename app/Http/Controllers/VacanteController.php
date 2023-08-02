75e7763f210429d9f7b3c6822696e4d6b7e81f71
esbelio <edgarestigarribia@gmail.com>
2023-08-02 19:49:39 -0400

    Edicion Vacante

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -49,6 +49,8 @@ class VacanteController extends Controller
     public function edit(Vacante $vacante)
     {
         //
+        $this->authorize('update', $vacante);
+
         return view('vacantes.edit',[
             'vacante' => $vacante
         ]);
