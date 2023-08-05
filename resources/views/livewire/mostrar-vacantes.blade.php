541efbb10ad6f9f63c8c4057e2f45cf59c0be84a
esbelio <edgarestigarribia@gmail.com>
2023-08-05 00:08:45 -0400

    mostrarVacante

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -20,10 +20,10 @@
                         Editar
                     </a>
 
-                    <a href="#" 
+                    <button wire:click="$emit('mostrarAlerta', {{ $vacante->id }})" 
                         class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
                         Eliminar
-                    </a>
+                </button>
                 </div>
 
             </div>
@@ -45,24 +45,30 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>    
 
     <script>
-        Swal.fire({
-            title: '¿Eliminar Vacante?',
-            text: "Una vacante eliminada no se puede recuperar!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, ¡Eliminar!',
-            cancelButtonText: 'Cancelar'
-        }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            )
-        }
+        Livewire.on('mostrarAlerta', vacanteId => {
+            Swal.fire({
+                title: '¿Eliminar Vacante?',
+                text: "Una vacante eliminada no se puede recuperar!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, ¡Eliminar!',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                //Eliminar la vacante
+                Livewire.emit('eliminarVacante', vacanteId)
+
+                Swal.fire(
+                    'Se eliminó la vacante!',
+                    'Eliminado Correctamente.',
+                    'success'
+                )
+            }
+            })
         })
+        
     </script>
 
 @endpush
