17630f97d5a5e6219c2437d58242d05d94294db9
Edgar Estigarribia <edgarestigarribia@hotmail.com>
2023-08-21 15:53:52 -0400

    Candidato

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -10,8 +10,9 @@
                 </div>
 
                 <div class="flex flex-col md:flex-row items-stretch gap-3 mt-4 md:mt-0">
-                    <a href="#" 
+                    <a href="{{ route('candidatos.index', $vacante) }}" 
                         class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        {{ $vacante->candidato->count() }}
                         Candidatos
                     </a>
                     
--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -24,7 +24,9 @@
                                 </p>
                             </div>
                             <div class="mt-5 lg:mt-0">
-                                <a href="#" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">Ver candidatos</a>
+                                <a href="{{ route('candidatos.index', $notificacion->data['id_vacante'])}}" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">
+                                    Ver candidatos
+                                </a>
                             </div>
                         </div>
                     @empty
